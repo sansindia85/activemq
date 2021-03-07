2e4968c61ac2733d5b5cfe863f8a5718ae8e80a4
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-03-07 20:03:23 +0530

    ActiveMQTopic : The topic messages are only sent to active subscribers.

---
+++ ActiveMQTopic/Subscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Apache.NMS;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Waiting for messages");
+
+            //Read all messages off the queue.
+            while (ReadNextMessage())
+            {
+                Console.WriteLine("Successfully read message.");
+            }
+
+            Console.WriteLine("Finished");
+        }
+
+        private static bool ReadNextMessage()
+        {
+            const string topic = "TestTextTopic";
+            
+            var brokerUri = $"amqp://guest:guest@localhost:5672";
+
+            var connectionFactory = new NMSConnectionFactory(brokerUri);
+
+            using var connection = connectionFactory.CreateConnection();
+            connection.Start();
+
+            using var session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            using var destination = session.GetTopic(topic);
+            using var consumer = session.CreateConsumer(destination);
+
+            var message = consumer.Receive();
+            if (message is ITextMessage)
+            {
+                var textMessage = message as ITextMessage;
+                Console.WriteLine($"Received message: {textMessage}");
+
+                return true;
+            }
+
+            Console.WriteLine("Unexpected message type: " + message.GetType().Name);
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 SenderReceiver - Hello AMQP. Uses Queues.
+
+SenderReceiverAdvanced - Hello AMQP using Asynchronous. Uses Queues.
+
+ActiveMQTopic - The topic messages are only sent to active subscribers. The messages are lost if no active subscribers.
+
+
