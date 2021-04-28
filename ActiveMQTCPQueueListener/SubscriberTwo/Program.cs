6a88649a74e7e303ec11cfdbe3dace4cc327343b
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-04-28 13:24:19 +0530

    Active MQ behavior with one publisher and two subscribers.

---
+++ ActiveMQTCPQueueListener/SubscriberTwo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Apache.NMS;
+
+namespace SubscriberTwo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Waiting for messages : Subscriber Two");
+
+            //Read all messages off the queue.
+            ReadNextMessage();
+
+            Console.WriteLine("Successfully read message.");
+            Console.WriteLine("Finished");
+        }
+
+        private static void ReadNextMessage()
+        {
+            const string queueName = "TestTextQueue";
+
+            var brokerUri = $"activemq:tcp://localhost:61616";
+
+            var connectionFactory = new NMSConnectionFactory(brokerUri);
+
+            using var connection = connectionFactory.CreateConnection("sans-app", "password");
+            connection.ClientId = "SubscribeTwo";
+            connection.Start();
+
+            using var session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            using var destination = session.GetQueue(queueName);
+            using var consumer = session.CreateConsumer(destination);
+
+            consumer.Listener += Consumer_Listener;
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Listener(IMessage message)
+        {
+            if (message is ITextMessage textMessage)
+                Console.WriteLine($"Received message: {textMessage.Text}");
+        }
+    }
+}
