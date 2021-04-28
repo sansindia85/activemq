9d16f08708046a2be91e7c7053cce7a24e35ec98
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-04-28 13:48:34 +0530

    ActiveMQ with Wild Cards.

---
+++ ActiveMQTCPQueueWildCard/Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Apache.NMS;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                var textMessage = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(textMessage))
+                    return;
+                SendNewMessage(textMessage);
+            }
+        }
+
+        private static void SendNewMessage(string textMessage)
+        {
+            string queueName = $"{Guid.NewGuid()}.TestTextQueue";
+            Console.WriteLine($"Add messaging to queue : {queueName}");
+
+            var brokerUri = $"activemq:tcp://localhost:61616";
+
+            var connectionFactory = new NMSConnectionFactory(brokerUri);
+
+            using var connection = connectionFactory.CreateConnection("sans-app", "password");
+            connection.ClientId = "Publisher";
+            connection.Start();
+
+            using var session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            using var destination = session.GetQueue(queueName);
+            using var producer = session.CreateProducer(destination);
+            producer.DeliveryMode = MsgDeliveryMode.Persistent;
+
+            producer.Send(session.CreateTextMessage(textMessage));
+            Console.WriteLine($"Sent {textMessage} messages");
+        }
+    }
+}
