214d1da190088a928ec2963e7b00fa5850282d0c
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-04-28 10:54:04 +0530

    Active mQ with TCP listener and multiple subscribers.

---
+++ ActiveMQTopicTCPListener/Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Apache.NMS;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string textMessage = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(textMessage))
+                    return;
+                SendNewMessage(textMessage);
+            }
+        }
+
+        private static void SendNewMessage(string textMessage)
+        {
+            const string topic = "TestTextTopic";
+            Console.WriteLine($"Add messaging to queue topic: {topic}");
+
+            var  brokerUri = $"activemq:tcp://localhost:61616";
+
+            var connectionFactory = new NMSConnectionFactory(brokerUri);
+
+            using var connection = connectionFactory.CreateConnection("sans-app", "password");
+            connection.Start();
+
+            using var session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            using var destination = session.GetTopic(topic);
+            using var producer = session.CreateProducer(destination);
+            producer.DeliveryMode = MsgDeliveryMode.Persistent;
+
+            producer.Send(session.CreateTextMessage(textMessage));
+            Console.WriteLine($"Sent {textMessage} messages");
+        }
+    }
+}
---
+++ ActiveMQTopicTCPListener/Subscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Apache.NMS;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Waiting for messages (Subscriber One)");
+
+            //Read all messages off the queue.
+            while (ReadNextMessage())
+            {
+                Console.WriteLine("Successfully read message.");
+            }
+
+            Console.WriteLine("Finished");
+        }
+
+        private static bool ReadNextMessage()
+        {
+            const string topic = "TestTextTopic";
+            
+            var brokerUri = $"activemq:tcp://localhost:61616";
+
+            var connectionFactory = new NMSConnectionFactory(brokerUri);
+
+            using var connection = connectionFactory.CreateConnection("sans-app", "password");
+            connection.Start();
+
+            using var session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            using var destination = session.GetTopic(topic);
+            using var consumer = session.CreateConsumer(destination);
+
+            var message = consumer.Receive();
+            if (message is ITextMessage)
+            {
+                var textMessage = message as ITextMessage;
+                Console.WriteLine($"Received message: {textMessage.Text}");
+
+                return true;
+            }
+
+            Console.WriteLine("Unexpected message type: " + message.GetType().Name);
+            return false;
+        }
+    }
+}
